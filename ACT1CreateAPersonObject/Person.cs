f59c198bbfa9caeceaef930362e4433bdcc29cb9
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-01-29 13:59:52 +0100

    Made some progress on activity 1 and also want to try to veryfy dates with TryParse

---
+++ ACT1CreateAPersonObject/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace ACT1CreateAPersonObject
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private string birthDate;
+        private float height;
+        private readonly string[] dateFormats ={"M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
+                                               "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
+                                               "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
+                                               "M/d/yyyy h:mm", "M/d/yyyy h:mm",
+                                               "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm"};
+        private readonly string ActualCulture = "en-US";
+        public Person(string name, string surname, string birthDate, float height)
+        {
+
+        }
+
+        public string GetName() { return this.name; }
+        public string GetSurname() { return this.surname; }
+        public string GetBirthDate() { return this.birthDate; }
+        public float GetHeight() { return this.height; }
+        public void SetName(string name) {this.name = name;}
+        public void SetSurname(string surname) {this.surname = surname;}
+        public void SetDate(string date) 
+        {
+            DateTime dateValue;
+            DateTime.TryParse(date, out dateValue);
+            this.birthDate = date;
+        }
+    }
+}
